08b640311afc982fdf9c06dd6bdf07fc761fa2cc
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-02-20 18:30:53 -0300

    New kata

---
+++ C#/7kyu/MoneyMoneyMoney.cs
@@ -0,0 +1,15 @@
+﻿namespace _7kyu;
+public static class MoneyMoneyMoney
+{
+    public static int CalculateYears(double principal, double interest, double tax, double desiredPrincipal)
+    {
+        var calculate = (ref double actual) => actual = actual + (actual * interest) - (actual * interest * tax);
+        var years = 0;
+        while (principal < desiredPrincipal)
+        {
+            calculate(ref principal);
+            years++;
+        }
+        return years;
+    }
+}
