88885315cf81ffdecf6a8012ef92372e4fabd724
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-08-26 18:13:07 -0300

    Kata from array fundamentals

---
+++ C#/6kyu/FindUniqueNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace _6kyu;
+public static class FindUniqueNumber
+{
+	public static int Solution(IEnumerable<int> numbers)
+	{
+		var reps = new Dictionary<int, int>();
+		foreach (var number in numbers) 
+		{
+			if (reps.ContainsKey(number))
+				reps[number]++;
+			else
+				reps[number] = 1;
+		}
+		return reps.First(x => x.Value == 1).Key;
+	}
+}
