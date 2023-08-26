ad5d0ebb145ca5f152de0d92e96a0fb1cd54d3a6
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-08-26 05:57:35 -0300

    Kata from datetime fundamentals

---
+++ C#/7kyu/HourFormatConvert.cs
@@ -0,0 +1,27 @@
+﻿namespace _7kyu;
+public static class HourFormatConvert
+{
+	public static string Solution(int hours, int minutes, string period)
+	{
+		var m = minutes.ToString().Length == 1 ? $"0{minutes}" : minutes.ToString();
+		string s;
+		if (period == "am")
+		{
+			s = hours switch
+			{
+				12 => "00",
+				11 or 10 => $"{hours}",
+				_ => $"0{hours}"
+			};
+		}
+		else
+		{
+			s = hours switch
+			{
+				12 => "12",
+				_ => $"{12 + hours}"
+			};
+		}
+		return s + m;
+	}
+}
