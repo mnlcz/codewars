e7b0b0d6123131ae7afcc646658869c0eddacd2b
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-08-26 05:14:59 -0300

    Solved kata from string fundamentals

---
+++ C#/7kyu/ReverseWords.cs
@@ -0,0 +1,8 @@
+﻿namespace _7kyu;
+public static class ReverseWords
+{
+	public static string Solution(string str)
+	{
+		return string.Join(' ', str.Split(' ').Select(s => new string(s.Reverse().ToArray())));
+	}
+}
